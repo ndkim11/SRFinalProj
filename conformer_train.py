9cc491dfcc2cd32cd7d5a7faaa145529d6c4f8e2
Sangyoon Kim <ndkim11@naver.com>
2024-05-31 00:54:18 +0900

    add current epoch number print

--- conformer_train.py
+++ conformer_train.py
@@ -471,7 +471,7 @@ def main():
     bestVloss = 100
     ## Train for args.max_epoch epochs
     for epoch in range(0, args.max_epoch):
-
+        print("Processing epoch {epoch}")
         # < fill your code here >
         tloss = process_epoch(model, trainloader, ctcloss, optimizer, trainmode=True)
         vloss = process_epoch(model, valloader, ctcloss, optimizer, trainmode=False)
